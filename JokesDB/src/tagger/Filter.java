0cb5815fcda46d5690fd0c348f4a6b524f5b1c8c
Xizhe Lian <xizhelian@googlemail.com>
2014-08-17 15:10:17 +0200

    add method to class filter

--- JokesDB/src/tagger/Filter.java
+++ JokesDB/src/tagger/Filter.java
@@ -2,8 +2,31 @@ package tagger;
 
 public class Filter {
 
+	/*
+	 * english or spanish?
+	 */
 	public void checkLanguage() {
 		
 	}
 
+	/*
+	 * too long?
+	 */
+	public void checkLength() {
+		
+	}
+	
+	/*
+	 * lawyer joke? sport? politic? blonde?
+	 */
+	public void addType() {
+		
+	}
+	
+	/*
+	 * too much?
+	 */
+	public void checkAppropriateness() {
+		
+	}
 }
